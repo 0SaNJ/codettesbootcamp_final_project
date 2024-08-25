e3334581d0a028f95fa345a55110de9e0625389b
0SaNJ <113564496+0SaNJ@users.noreply.github.com>
2024-08-24 22:41:29 -0300

    Update final_project_app.js

--- final_project_app.js
+++ final_project_app.js
@@ -1,59 +1,65 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";  // "mongodb://localhost:27017/iotstack";
 
-
 var express = require('express');
 var app = express();
 
-//Server Variables
+// Server Variables
 var host = process.env.IP || 'localhost';
-var port = process.env.PORT || 9000;
+var port = process.env.PORT || 8000;
 
-var staticSite = __dirname + '/.';
+var staticSite = __dirname + '/public';
 
-var mqtt = require('mqtt')
+var mqtt = require('mqtt');
 
-var client  = mqtt.connect([{host:'localhost',port:'1883'}]) //var client  = mqtt.connect([{host:'broker.hivemq.com',port:'1883'}]) //
- console.log("Nodejs Server Started!");
- 
-// on mqtt conect subscribe on tobic test 
+//var client = mqtt.connect([{host:'localhost',port:'9001'}]);
+const client = mqtt.connect('ws://192.168.1.18:9001/mqtt');
+console.log("Nodejs Server Started!");
+
+// Express middleware to serve static files
+app.use(express.static(staticSite));
+
+// Connect to MQTT broker and subscribe to topic
 client.on('connect', function () {
+  console.log("Connected to MQTT broker");
   client.subscribe('jaysanmo/codettes/01', function (err) {
-	 console.log("sub scribing to test topic");
-      if(err)
-      console.log(err)
-  })
-})
-
- //when recive message 
-client.on('message', function (topic, message) {
-  json_check(message)
-})
-
-//check if data json or not
-function json_check(data) {
-    try {
-       // JSON.parse(data);
-	 msg = JSON.parse(data.toString()); // t is JSON so handle it how u want
-    } catch (e) {
-		console.log("message could not valid json " + data.toString);
-        return false;
+    if (err) {
+      console.error("Failed to subscribe to MQTT topic:", err);
+    } else {
+      console.log("Subscribed to MQTT topic");
     }
-	 console.log(msg);
-	 var msgobj = { "msg": msg }; // message object
-    Mongo_insert(msgobj)
-	console.log(msgobj);
+  });
+});
+
+// Handle MQTT messages
+client.on('message', function (topic, message) {
+  console.log("Received message from MQTT:", message.toString());
+  json_check(message);
+});
+
+// Check if message is JSON
+function json_check(data) {
+  try {
+    var msg = JSON.parse(data.toString());
+    console.log("Parsed JSON message:", msg);
+    var msgobj = { "msg": msg }; // message object
+    Mongo_insert(msgobj);
+    console.log("Message object:", msgobj);
+  } catch (e) {
+    console.log("Message could not be parsed as JSON:", data.toString());
+    return false;
+  }
 }
 
-//insert data in mongodb
+// Insert data into MongoDB
 function Mongo_insert(msg){
-MongoClient.connect(url, function(err, db ) {
+  MongoClient.connect(url, function(err, db ) {
     if (err) throw err;
     var dbo = db.db("cb");
     dbo.collection("class").insertOne(msg, function(err, res) {
       if (err) throw err;
-	   console.log("data stored");
-      //db.close();
+      console.log("Data stored in MongoDB:", msg);
+      db.close();
     });
   }); 
 }
@@ -69,10 +75,8 @@ app.use(function(req, res, next) {
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
-
 app.use('/', express.static(staticSite));
 // Use router for all /api requests
-
 
 if (! process.env.C9_PID) {
     console.log('Running at http://'+ host +':' + port);
